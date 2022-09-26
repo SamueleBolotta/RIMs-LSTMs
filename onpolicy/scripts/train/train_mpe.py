bcf7f914f9665693a629fdf9344fa5b566795325
Samuele <bolottasamuele@gmail.com>
2022-09-26 15:47:31 +0200

    Update train_mpe.py

--- onpolicy/scripts/train/train_mpe.py
+++ onpolicy/scripts/train/train_mpe.py
@@ -8,46 +8,13 @@ import numpy as np
 from pathlib import Path
 import torch
 from onpolicy.config import get_config
-from onpolicy.envs.mpe.MPE_env import MPEEnv
-from onpolicy.envs.env_wrappers import SubprocVecEnv, DummyVecEnv
+from pettingzoo.mpe import simple_spread_v2
+from pettingzoo.mpe._mpe_utils.core import Agent, Landmark, World
+from pettingzoo.mpe._mpe_utils.scenario import BaseScenario
+from pettingzoo.mpe._mpe_utils.simple_env import SimpleEnv, make_env
+from pettingzoo.utils.conversions import parallel_wrapper_fn
 
 """Train script for MPEs."""
-
-def make_train_env(all_args):
-    def get_env_fn(rank):
-        def init_env():
-            if all_args.env_name == "MPE":
-                env = MPEEnv(all_args)
-            else:
-                print("Can not support the " +
-                      all_args.env_name + "environment.")
-                raise NotImplementedError
-            env.seed(all_args.seed + rank * 1000)
-            return env
-        return init_env
-    if all_args.n_rollout_threads == 1:
-        return DummyVecEnv([get_env_fn(0)])
-    else:
-        return SubprocVecEnv([get_env_fn(i) for i in range(all_args.n_rollout_threads)])
-
-
-def make_eval_env(all_args):
-    def get_env_fn(rank):
-        def init_env():
-            if all_args.env_name == "MPE":
-                env = MPEEnv(all_args)
-            else:
-                print("Can not support the " +
-                      all_args.env_name + "environment.")
-                raise NotImplementedError
-            env.seed(all_args.seed * 50000 + rank * 10000)
-            return env
-        return init_env
-    if all_args.n_eval_rollout_threads == 1:
-        return DummyVecEnv([get_env_fn(0)])
-    else:
-        return SubprocVecEnv([get_env_fn(i) for i in range(all_args.n_eval_rollout_threads)])
-
 
 def parse_args(args, parser):
     parser.add_argument('--scenario_name', type=str,
@@ -129,10 +96,12 @@ def main(args):
     np.random.seed(all_args.seed)
 
     # env init
-    envs = make_train_env(all_args)
-    eval_envs = make_eval_env(all_args) if all_args.use_eval else None
+    envs = simple_spread_v2.env(N=3, local_ratio=0.5, max_cycles=25, continuous_actions=False)
+    eval_envs = simple_spread_v2.env(N=3, local_ratio=0.5, max_cycles=25, continuous_actions=False) if all_args.use_eval else None
     num_agents = all_args.num_agents
-
+    
+    print(envs)
+    
     config = {
         "all_args": all_args,
         "envs": envs,
