336700d3b45d5e9317e92b0ae2ffa02feed93129
Samuele <bolottasamuele@gmail.com>
2022-09-26 15:38:05 +0200

    Update r_mappo.py

--- onpolicy/algorithms/r_mappo/r_mappo.py
+++ onpolicy/algorithms/r_mappo/r_mappo.py
@@ -101,7 +101,7 @@ class R_MAPPO():
         :return actor_grad_norm: (torch.Tensor) gradient norm from actor update.
         :return imp_weights: (torch.Tensor) importance sampling weights.
         """
-        share_obs_batch, obs_batch, rnn_states_batch, rnn_states_critic_batch, actions_batch, \
+        obs_batch, rnn_states_batch, rnn_states_critic_batch, actions_batch, \
         value_preds_batch, return_batch, masks_batch, active_masks_batch, old_action_log_probs_batch, \
         adv_targ, available_actions_batch = sample
 
@@ -112,8 +112,7 @@ class R_MAPPO():
         active_masks_batch = check(active_masks_batch).to(**self.tpdv)
 
         # Reshape to do in a single forward pass for all steps
-        values, action_log_probs, dist_entropy = self.policy.evaluate_actions(share_obs_batch,
-                                                                              obs_batch, 
+        values, action_log_probs, dist_entropy = self.policy.evaluate_actions(obs_batch, 
                                                                               rnn_states_batch, 
                                                                               rnn_states_critic_batch, 
                                                                               actions_batch, 
