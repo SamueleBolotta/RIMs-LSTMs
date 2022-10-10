2fb2c3e509b8872eac088ff69b0377bd1b3ffe9a
Samuele <bolottasamuele@gmail.com>
2022-10-10 17:29:33 +0200

    Update separated_buffer.py

--- onpolicy/utils/separated_buffer.py
+++ onpolicy/utils/separated_buffer.py
@@ -315,10 +315,8 @@ class SeparatedReplayBuffer(object):
         returns = _cast(self.returns[:-1])
         masks = _cast(self.masks[:-1])
         active_masks = _cast(self.active_masks[:-1])
-        print("rnn states shape", self.rnn_states.shape)
         rnn_states = self.rnn_states[:-1].transpose(1, 0, 2, 3).reshape(-1, *self.rnn_states.shape[2:])
         rnn_states_critic = self.rnn_states_critic[:-1].transpose(1, 0, 2, 3).reshape(-1, *self.rnn_states_critic.shape[2:])
-        print("rnn states shape", rnn_states.shape)
 
         if self.available_actions is not None:
             available_actions = _cast(self.available_actions[:-1])
