ae9e5a576dbaaccb7e2c285ca75279744d24131e
Samuele <bolottasamuele@gmail.com>
2022-11-14 20:17:56 +0100

    Update train_multiwalker.py

--- onpolicy/scripts/train/train_multiwalker.py
+++ onpolicy/scripts/train/train_multiwalker.py
@@ -136,7 +136,7 @@ def main(args):
         if all_args.n_rollout_threads == 1: 
             from onpolicy.runner.separated.mw.multiwalker_runner_mono import SISLRunner as Runner
         else:
-            from onpolicy.runner.separated.mw.ultiwalker_runner_multi import SISLRunner as Runner
+            from onpolicy.runner.separated.mw.multiwalker_runner_multi import SISLRunner as Runner
 
     runner = Runner(config)
     runner.run()
