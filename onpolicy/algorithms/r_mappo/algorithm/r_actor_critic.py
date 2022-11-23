c5037a7f4c407580287c7a0e5b09313c9f782fa4
Samuele <bolottasamuele@gmail.com>
2022-11-23 15:53:24 +0100

    Update r_actor_critic.py

--- onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
+++ onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -5,7 +5,8 @@ from onpolicy.algorithms.utils.util import init, check
 from onpolicy.algorithms.utils.cnn import CNNBase
 from onpolicy.algorithms.utils.mlp import MLPBase
 from onpolicy.algorithms.utils.rnn import RNNLayer
-from onpolicy.algorithms.utils.act import ACTLayer
+from onpolicy.algorithms.utils.multiwalker_act import ACTLayer as multiwalker_act
+from onpolicy.algorithms.utils.mpe_act import ACTLayer as mpe_act
 from onpolicy.algorithms.utils.popart import PopArt
 from onpolicy.utils.util import get_shape_from_obs_space
 from onpolicy.algorithms.utils.RIM import RIMCell
@@ -21,7 +22,8 @@ class R_Actor(nn.Module):
     def __init__(self, args, obs_space, action_space, device=torch.device("cpu")):
         super(R_Actor, self).__init__()
         self.hidden_size = args.hidden_size
-
+        self._env_name = args.env_name
+        print("environment: ", self._env_name)
         self._gain = args.gain
         self._use_orthogonal = args.use_orthogonal
         self._use_policy_active_masks = args.use_policy_active_masks
@@ -63,7 +65,11 @@ class R_Actor(nn.Module):
                 self.rnn = RIMCell(torch.device('cuda' if torch.cuda.is_available() else 'cpu'), self.hidden_size, self.hidden_size // self._num_units, self._num_units, 1, 'GRU', input_value_size = 64, comm_value_size = self.hidden_size // self._num_units)
             elif self._use_lstm_policy:
                 self.rnn = RNNLayer(self.hidden_size, self.hidden_size, self._recurrent_N, self._use_orthogonal)
-        self.act = ACTLayer(action_space, self.hidden_size, self._use_orthogonal, self._gain)
+                
+        if self._env_name == 'MPE-simple.spread':
+            self.act = mpe_act(action_space, self.hidden_size, self._use_orthogonal, self._gain)
+        elif self._env_name == 'SISL-multiwalker':
+            self.act = multiwalker_act(action_space, self.hidden_size, self._use_orthogonal, self._gain)
 
         self.to(device)
         
