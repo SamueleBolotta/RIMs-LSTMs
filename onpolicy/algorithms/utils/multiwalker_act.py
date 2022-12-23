4e587c1dbef52d65221e2add23546b6db27ad3a3
Samuele <bolottasamuele@gmail.com>
2022-12-23 23:33:48 +0100

    Update multiwalker_act.py

--- onpolicy/algorithms/utils/multiwalker_act.py
+++ onpolicy/algorithms/utils/multiwalker_act.py
@@ -16,29 +16,12 @@ class ACTLayer(nn.Module):
         self.multi_discrete = False
         self.action_space = action_space
         self.m = nn.Tanh()
+        
+        print("action_space", action_space)
+        action_dim = action_space.shape[0]
+        print("action_dim", action_dim)
+        self.action_out = DiagGaussian(inputs_dim, action_dim, use_orthogonal, gain)
 
-        if action_space.__class__.__name__ == "Discrete":
-            action_dim = action_space.n
-            self.action_out = Categorical(inputs_dim, action_dim, use_orthogonal, gain)
-        elif action_space.__class__.__name__ == "Box":
-            action_dim = action_space.shape[0]
-            self.action_out = DiagGaussian(inputs_dim, action_dim, use_orthogonal, gain)
-        elif action_space.__class__.__name__ == "MultiBinary":
-            action_dim = action_space.shape[0]
-            self.action_out = Bernoulli(inputs_dim, action_dim, use_orthogonal, gain)
-        elif action_space.__class__.__name__ == "MultiDiscrete":
-            self.multi_discrete = True
-            action_dims = action_space.high - action_space.low + 1
-            self.action_outs = []
-            for action_dim in action_dims:
-                self.action_outs.append(Categorical(inputs_dim, action_dim, use_orthogonal, gain))
-            self.action_outs = nn.ModuleList(self.action_outs)
-        else:  # discrete + continous
-            self.mixed_action = True
-            continous_dim = action_space[0].shape[0]
-            discrete_dim = action_space[1].n
-            self.action_outs = nn.ModuleList([DiagGaussian(inputs_dim, continous_dim, use_orthogonal, gain), Categorical(
-                inputs_dim, discrete_dim, use_orthogonal, gain)])
     
     def forward(self, x, available_actions=None, deterministic=False):
         """
@@ -69,16 +52,9 @@ class ACTLayer(nn.Module):
 
         :return action_probs: (torch.Tensor)
         """
-        if self.mixed_action or self.multi_discrete:
-            action_probs = []
-            for action_out in self.action_outs:
-                action_logit = action_out(x)
-                action_prob = action_logit.probs
-                action_probs.append(action_prob)
-            action_probs = torch.cat(action_probs, -1)
-        else:
-            action_logits = self.action_out(x)
-            action_probs = action_logits.probs
+
+        action_logits = self.action_out(x)
+        action_probs = action_logits.probs
         
         return action_probs
 
@@ -94,47 +70,15 @@ class ACTLayer(nn.Module):
         :return action_log_probs: (torch.Tensor) log probabilities of the input actions.
         :return dist_entropy: (torch.Tensor) action distribution entropy for the given inputs.
         """
-        if self.mixed_action:
-            a, b = action.split((2, 1), -1)
-            b = b.long()
-            action = [a, b] 
-            action_log_probs = [] 
-            dist_entropy = []
-            for action_out, act in zip(self.action_outs, action):
-                action_logit = action_out(x)
-                action_log_probs.append(action_logit.log_probs(act))
-                if active_masks is not None:
-                    if len(action_logit.entropy().shape) == len(active_masks.shape):
-                        dist_entropy.append((action_logit.entropy() * active_masks).sum()/active_masks.sum()) 
-                    else:
-                        dist_entropy.append((action_logit.entropy() * active_masks.squeeze(-1)).sum()/active_masks.sum())
-                else:
-                    dist_entropy.append(action_logit.entropy().mean())
-                
-            action_log_probs = torch.sum(torch.cat(action_log_probs, -1), -1, keepdim=True)
-            dist_entropy = dist_entropy[0] / 2.0 + dist_entropy[1] / 0.98 #! dosen't make sense
 
-        elif self.multi_discrete:
-            action = torch.transpose(action, 0, 1)
-            action_log_probs = []
-            dist_entropy = []
-            for action_out, act in zip(self.action_outs, action):
-                action_logit = action_out(x)
-                action_log_probs.append(action_logit.log_probs(act))
-                if active_masks is not None:
-                    dist_entropy.append((action_logit.entropy()*active_masks.squeeze(-1)).sum()/active_masks.sum())
-                else:
-                    dist_entropy.append(action_logit.entropy().mean())
+        print("x", x)
+        action_logits = self.action_out(x)
+        print("action_logits", x)
 
-            action_log_probs = torch.cat(action_log_probs, -1) # ! could be wrong
-            dist_entropy = sum(dist_entropy)/len(dist_entropy)
-        
+        action_log_probs = action_logits.log_probs(action)
+        if active_masks is not None:
+            dist_entropy = (action_logits.entropy()*active_masks.squeeze(-1)).sum()/active_masks.sum()
         else:
-            action_logits = self.action_out(x)
-            action_log_probs = action_logits.log_probs(action)
-            if active_masks is not None:
-                dist_entropy = (action_logits.entropy()*active_masks.squeeze(-1)).sum()/active_masks.sum()
-            else:
-                dist_entropy = action_logits.entropy().mean()
+            dist_entropy = action_logits.entropy().mean()
         
         return action_log_probs, dist_entropy
